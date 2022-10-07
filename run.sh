25fd89b6befb4055ed5f91f2d69f227c2b525ddb
alix.gao <alix.gao@qq.com>
2022-10-07 23:19:07 +0800

    adjust timer interrupt & add dts for booting kernel
    Signed-off-by: alix.gao <alix.gao@qq.com>

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 ./qemu/build/qemu-system-aarch64 -machine baize -nographic \
 	-bios ./boot.rom \
 	-drive if=pflash,format=raw,index=0,file=./pflash.raw \
-	-drive if=none,file=rootfs.ext4,id=hd0 \
+	-drive if=none,file=disk.rootfs,id=hd0 \
 	-device virtio-blk-device,drive=hd0 \
 	#-S -s
 
