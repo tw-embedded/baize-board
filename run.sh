fac2e47a369b55228fe6b28fde67dc0fc9f7cb20
alix <alix.gao@qq.com>
2026-02-07 12:15:51 +0000

    fix: run without android
    Signed-off-by: alix <alix.gao@qq.com>

--- run.sh
+++ run.sh
@@ -1,9 +1,23 @@
-if [ -z $1 ]; then
-	echo "no debug"
-	DEBUG=""
-else
-	DEBUG="-S -s"
-fi
+#!/bin/bash
+
+DEBUG=""
+ANDROID_DRIVE=""
+
+for arg in "$@"; do
+    case $arg in
+        -d|d|debug)
+            echo "debug mode..."
+            DEBUG="-S -s"
+            ;;
+        android)
+            echo "using android rootfs..."
+            ANDROID_DRIVE="-drive if=none,file=android.rootfs,id=hd3 -device virtio-blk-device,drive=hd3 "
+            ;;
+        *)
+            echo "run with default..."
+     	    ;;
+    esac
+done
 
 #gdb --args \
 ./qemu/build/qemu-system-aarch64 -machine baize -nographic \
@@ -11,8 +25,7 @@ fi
 	-drive if=pflash,format=raw,index=0,file=./pflash.raw \
 	-drive if=none,file=emmc.img,id=hd2 \
 	-device virtio-blk-device,drive=hd2 \
-	-drive if=none,file=android.rootfs,id=hd3 \
-        -device virtio-blk-device,drive=hd3 \
+	${ANDROID_DRIVE} \
 	-drive if=none,file=domu.rootfs,id=hd1 \
 	-device virtio-blk-device,drive=hd1 \
 	-drive if=none,file=dom0.rootfs,id=hd0 \
