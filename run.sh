74f9cc14f0d4126bf84c1d13e2ef920d29ce6386
alix <alix.gao@qq.com>
2026-07-05 03:18:37 +0000

    [codex] Support split GTK windows for dual virtio-gpu display
      Run QEMU UI mode with two virtio-gpu devices and split the GTK display
      into two windows. The first GPU remains 640x480, while the second GPU is
      800x600 and detached as a separate window.
    
      Move Android GPU passthrough from virtio-mmio 0x20002e00/IRQ57 to
      0x20002c00/IRQ56 so Android uses the 800x600 display. Update android.cfg,
      virtio-gpu.dts, xen.dts, and regenerate the corresponding DTBs.
    
      Add QEMU GTK helpers for forced per-console window sizing and automatic
      detach of selected graphic consoles. Also ignore too-small GTK UI size
      updates in virtio-gpu to avoid early firmware seeing invalid console sizes.
    
      Use gtk,gl=off for now because the GTK OpenGL path does not reliably render
      the detached Android window after tab/window changes.
    
    Signed-off-by: izaac <izaac.gao@qq.com>

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 DEBUG=""
+QEMU_ENV=""
 
 # unsafe for performance optimization
 ANDROID_DRIVE="-drive if=none,file=android.rootfs,id=hd3,cache=unsafe -device virtio-blk-device,drive=hd3"
@@ -14,9 +15,11 @@ for arg in "$@"; do
             ;;
 	-v|v) # for EC2 & android
 	    DISPLAY_OPT="-device virtio-gpu-device -display none -vnc :1 -serial stdio"
+	    QEMU_ENV=""
 	    ;;
         -u|u|ui)
-            DISPLAY_OPT="-device virtio-gpu-device,edid=off,xres=800,yres=600,blob=off -device virtio-gpu-device,edid=on,xres=640,yres=480,blob=off -m 2G -display gtk,gl=on -serial stdio"
+            DISPLAY_OPT="-device virtio-gpu-device,edid=off,xres=640,yres=480,blob=off -device virtio-gpu-device,edid=on,xres=800,yres=600,blob=off -m 2G -display gtk,gl=off -serial stdio"
+            QEMU_ENV="QEMU_GTK_DETACH_GFX=second QEMU_GTK_GFX_SIZES=640x480,800x600"
             #DISPLAY_OPT="-device virtio-gpu-gl-device -vnc :29 -monitor stdio"
             ;;
         *)
@@ -26,7 +29,7 @@ for arg in "$@"; do
 done
 
 #gdb --args \
-./qemu/build/qemu-system-aarch64 -machine baize \
+env ${QEMU_ENV} ./qemu/build/qemu-system-aarch64 -machine baize \
 	${DISPLAY_OPT} \
 	-bios ./boot.rom \
 	-drive if=pflash,format=raw,index=0,file=./pflash.raw \
@@ -38,4 +41,3 @@ done
 	-drive if=none,file=dom0.rootfs,id=hd0 \
 	-device virtio-blk-device,drive=hd0 \
 	${DEBUG}
-
